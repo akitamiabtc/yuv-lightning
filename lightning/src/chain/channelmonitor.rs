c3f3c84a91ebeb62473aa0bf2177dea31fd2c93a
Wilmer Paulino <wilmer@wilmerpaulino.com>
2023-10-16 12:21:52 -0700

    Move HTLCDescriptor to sign module
    Now that `HTLCDescriptor` is no longer specific to anchors, it doesn't
    make sense for it to live in the `bump_transaction` module anymore.

--- lightning/src/chain/channelmonitor.rs
+++ lightning/src/chain/channelmonitor.rs
@@ -43,7 +43,7 @@ use crate::chain;
 use crate::chain::{BestBlock, WatchedOutput};
 use crate::chain::chaininterface::{BroadcasterInterface, FeeEstimator, LowerBoundedFeeEstimator, YuvBroadcaster};
 use crate::chain::transaction::{OutPoint, TransactionData};
-use crate::sign::{SpendableOutputDescriptor, StaticPaymentOutputDescriptor, DelayedPaymentOutputDescriptor, WriteableEcdsaChannelSigner, SignerProvider, EntropySource, DelayedYuvPaymentOutputDescriptor, StaticYuvPaymentOutputDescriptor, YuvStaticOutputDescriptor};
+use crate::sign::{ChannelDerivationParameters, HTLCDescriptor, SpendableOutputDescriptor, StaticPaymentOutputDescriptor, DelayedPaymentOutputDescriptor, WriteableEcdsaChannelSigner, SignerProvider, EntropySource};
 use crate::chain::onchaintx::{ClaimEvent, OnchainTxHandler};
 use crate::chain::package::{CounterpartyOfferedHTLCOutput, CounterpartyReceivedHTLCOutput, HolderFundingOutput, HolderHTLCOutput, PackageSolvingData, PackageTemplate, RevokedOutput, RevokedHTLCOutput, get_yuv_htlc_pixel_proof};
 use crate::chain::Filter;
@@ -51,7 +51,7 @@ use crate::util::logger::Logger;
 use crate::util::ser::{Readable, ReadableArgs, RequiredWrapper, MaybeReadable, UpgradableRequired, Writer, Writeable, U48};
 use crate::util::byte_utils;
 use crate::events::{Event, EventHandler};
-use crate::events::bump_transaction::{ChannelDerivationParameters, AnchorDescriptor, HTLCDescriptor, BumpTransactionEvent};
+use crate::events::bump_transaction::{AnchorDescriptor, BumpTransactionEvent};
 
 use crate::prelude::*;
 use core::{cmp, mem};
