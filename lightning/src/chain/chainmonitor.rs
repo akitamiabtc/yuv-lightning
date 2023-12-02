51b024b116713e2484d75aef0e18ae7aff123dd7
Matt Corallo <git@bluematt.me>
2023-12-02 17:54:18 +0000

    Correct lock drop'ing in `ChainMonitor::update_channel`
    e21a500668179c7084e2df5cb75019810eb03cbe cleaned up the error
    handling in `ChainMonitor::update_channel` a bit, but accidentally
    replaced the deliberate panic with a hang. This commit ensures we
    properly drop the monitors read lock before taking a write lock.

--- lightning/src/chain/chainmonitor.rs
+++ lightning/src/chain/chainmonitor.rs
@@ -818,8 +818,7 @@ where
 
 	fn update_channel(&self, funding_txo: OutPoint, update: &ChannelMonitorUpdate) -> ChannelMonitorUpdateStatus {
 		// Update the monitor that watches the channel referred to by the given outpoint.
-		let monitors_lock = self.monitors.read().unwrap();
-		let monitors = monitors_lock.deref();
+		let monitors = self.monitors.read().unwrap();
 		match monitors.get(&funding_txo) {
 			None => {
 				log_error!(self.logger, "Failed to update channel monitor: no such monitor registered");
@@ -862,6 +861,7 @@ where
 					ChannelMonitorUpdateStatus::UnrecoverableError => {
 						// Take the monitors lock for writing so that we poison it and any future
 						// operations going forward fail immediately.
+						core::mem::drop(pending_monitor_updates);
 						core::mem::drop(monitors);
 						let _poison = self.monitors.write().unwrap();
 						let err_str = "ChannelMonitor[Update] persistence failed unrecoverably. This indicates we cannot continue normal operation and must shut down.";
