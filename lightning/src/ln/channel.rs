4b4ffb1d4fcd6e2c699639909a27edd09af8a9be
Matt Corallo <git@bluematt.me>
2023-12-29 03:15:18 +0000

    Move DiscardFunding generation into finish_close_channel
    Currently the channel shutdown sequence has a number of steps which
    all the shutdown callsites have to call. Because many shutdown
    cases are rare error cases, its relatively easy to miss a call and
    leave users without `Event`s or miss some important cleanup.
    
    One of those steps, calling `issue_channel_close_events`, is rather
    easy to remove, as it only generates two events, which can simply
    be moved to another shutdown step.
    
    Here we move the first of the two events, `DiscardFunding`, into
    `finish_force_close_channel`.

--- lightning/src/ln/channel.rs
+++ lightning/src/ln/channel.rs
@@ -865,6 +865,7 @@ pub(crate) struct ShutdownResult {
 	pub(crate) unbroadcasted_batch_funding_txid: Option<Txid>,
 	pub(crate) channel_id: ChannelId,
 	pub(crate) counterparty_node_id: PublicKey,
+	pub(crate) unbroadcasted_funding_tx: Option<Transaction>,
 }
 
 /// If the majority of the channels funds are to the fundee and the initiator holds only just
@@ -2883,6 +2884,7 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider  {
 			} else { None }
 		} else { None };
 		let unbroadcasted_batch_funding_txid = self.unbroadcasted_batch_funding_txid();
+		let unbroadcasted_funding_tx = self.unbroadcasted_funding();
 
 		self.channel_state = ChannelState::ShutdownComplete;
 		self.update_time_counter += 1;
@@ -2892,6 +2894,7 @@ impl<SP: Deref> ChannelContext<SP> where SP::Target: SignerProvider  {
 			unbroadcasted_batch_funding_txid,
 			channel_id: self.channel_id,
 			counterparty_node_id: self.counterparty_node_id,
+			unbroadcasted_funding_tx,
 		}
 	}
 
@@ -5536,6 +5539,7 @@ impl<SP: Deref> Channel<SP> where
 					unbroadcasted_batch_funding_txid: self.context.unbroadcasted_batch_funding_txid(),
 					channel_id: self.context.channel_id,
 					counterparty_node_id: self.context.counterparty_node_id,
+					unbroadcasted_funding_tx: self.context.unbroadcasted_funding(),
 				};
 				let tx = self.build_signed_closing_transaction(&mut closing_tx, &msg.signature, &sig);
 				self.context.channel_state = ChannelState::ShutdownComplete;
@@ -5580,6 +5584,7 @@ impl<SP: Deref> Channel<SP> where
 								unbroadcasted_batch_funding_txid: self.context.unbroadcasted_batch_funding_txid(),
 								channel_id: self.context.channel_id,
 								counterparty_node_id: self.context.counterparty_node_id,
+								unbroadcasted_funding_tx: self.context.unbroadcasted_funding(),
 							};
 							self.context.channel_state = ChannelState::ShutdownComplete;
 							self.context.update_time_counter += 1;
