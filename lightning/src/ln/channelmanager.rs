167c07eae8add2e682a86ec592d27ed7928dcb7a
Matt Corallo <git@bluematt.me>
2024-01-18 20:27:57 +0000

    Stop relying on `Hash{Set,Map}::from_iter` directly
    In the next commit we'll bump the `hashbrown` version, which no
    longer randomizes its hasher by default. Thus, we'll need to call
    a different constructor in no-std builds from std builds.
    
    Here we do a quick prefactor to use wrappers for `FromIterator`
    constructors instead of calling the tables directly to make the
    version bump changeset smaller.

--- lightning/src/ln/channelmanager.rs
+++ lightning/src/ln/channelmanager.rs
@@ -10885,8 +10885,10 @@ where
 	pub fn new(entropy_source: ES, node_signer: NS, signer_provider: SP, fee_estimator: F, chain_monitor: M, tx_broadcaster: T, router: R, logger: L, default_config: UserConfig,
 			mut channel_monitors: Vec<&'a mut ChannelMonitor<<SP::Target as SignerProvider>::EcdsaSigner>>) -> Self {
 		Self {
-			entropy_source, node_signer, signer_provider, fee_estimator, chain_monitor, tx_broadcaster, yuv_tx_broadcaster, router, logger, default_config,
-			channel_monitors: channel_monitors.drain(..).map(|monitor| { (monitor.get_funding_txo().0, monitor) }).collect()
+			entropy_source, node_signer, signer_provider, fee_estimator, chain_monitor, tx_broadcaster, router, logger, default_config,
+			channel_monitors: hash_map_from_iter(
+				channel_monitors.drain(..).map(|monitor| { (monitor.get_funding_txo().0, monitor) })
+			),
 		}
 	}
 }
@@ -11161,7 +11163,7 @@ where
 		for _ in 0..pending_outbound_payments_count_compat {
 			let session_priv = Readable::read(reader)?;
 			let payment = PendingOutboundPayment::Legacy {
-				session_privs: [session_priv].iter().cloned().collect()
+				session_privs: hash_set_from_iter([session_priv]),
 			};
 			if pending_outbound_payments_compat.insert(PaymentId(session_priv), payment).is_some() {
 				return Err(DecodeError::InvalidValue)
@@ -11391,7 +11393,7 @@ where
 										retry_strategy: None,
 										attempts: PaymentAttempts::new(),
 										payment_params: None,
-										session_privs: [session_priv_bytes].iter().map(|a| *a).collect(),
+										session_privs: hash_set_from_iter([session_priv_bytes]),
 										payment_hash: htlc.payment_hash,
 										payment_secret: None, // only used for retries, and we'll never retry on startup
 										payment_metadata: None, // only used for retries, and we'll never retry on startup
